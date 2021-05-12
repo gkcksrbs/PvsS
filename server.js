39e03ef1b09b1eb84ee8c8ad30367d562a645d14
unknown <gkcksrbs@ajou.ac.kr>
2021-05-13 03:15:29 +0900

    item

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 
-server.listen(8000, () => {
+server.listen(process.env.PORT || 8000, () => {
     console.log("서버가 대기중입니다.");
 })
 
@@ -62,31 +62,36 @@ function endGame(socket){
     delete ballMap[socket.id];
 }
 
-let enemyGenerator;
-let itemGenerator;
-let isStarted = false;
+
+let stageGenerator;
+let stage = 2;
+let isAccessFail = false;
+let enemyGeneratorInterval;
+let enemyFrequency = 800;
+let itemGeneratorInterval;
+let itemFrequency = 15000;
 
 io.on('connection', function(socket) {
-    
     console.log(`${socket.id}님이 입장하셨습니다.`);
 
-
     socket.on('disconnect', function(reason){
-        
         console.log(`${socket.id}님이 ${reason}의 이유로 퇴장하셨습니다. `)
         endGame(socket);
         io.sockets.emit('leave_user', socket.id);
         if(balls.length == 0){
-            clearInterval(enemyGenerator)
-            clearInterval(itemGenerator)
-            isStarted = false
+            clearInterval(enemyGeneratorInterval);
+            clearInterval(stageGenerator);
+            clearInterval(itemGeneratorInterval);
+            stage = 2;
+            isAccessFail = false;
         }
     });
 
+
+    
     let newBall = joinGame(socket);
 
     socket.emit('user_id', socket.id);
-
  
     for (var i = 0 ; i < balls.length; i++){
         let ball = balls[i];
@@ -105,7 +110,7 @@ io.on('connection', function(socket) {
         color: newBall.color,
     });
 
-    if(balls.length > 3 || isStarted){
+    if(balls.length > 3 || isAccessFail){//3명 이상 접속시 접속자 차단해버리기 
         console.log(socket.id)
         socket.emit('force_disconnect', socket.id);
         endGame(socket);
@@ -121,63 +126,76 @@ io.on('connection', function(socket) {
             })
     })
 
+    function enemyLeftSideGenerator(){
+            if(balls.length){
+                var randomStartY = Math.floor(Math.random() * 768)
+                var randomDestinationY = Math.floor(Math.random() * 768)
+                io.sockets.emit('enemy_generator', {
+                    wall : 0,
+                    startingX:  enemyRadius,
+                    startingY:  randomStartY,
+                    destinationX:  canvasWidth+enemyRadius,
+                    destinationY: randomDestinationY,
+                })
+            }
+    }
+    function enemyRightSideGenerator(){
+            if(balls.length){
+                var randomStartY = Math.floor(Math.random() * 768)
+                var randomDestinationY = Math.floor(Math.random() * 768)
+                io.sockets.emit('enemy_generator', {
+                    wall : 1,
+                    startingX:  canvasWidth+enemyRadius,
+                    startingY:  randomStartY,
+                    destinationX:  enemyRadius,
+                    destinationY: randomDestinationY,
+                })
+            }
+    }
+    function enemyUpSideGenerator(){
+            if(balls.length){
+                var randomStartX = Math.floor(Math.random() * 1024);
+                var randomDestinationX = Math.floor(Math.random() * 1024);
+                io.sockets.emit('enemy_generator', {
+                    wall : 2,
+                    startingX:  randomStartX,
+                    startingY:  enemyRadius,
+                    destinationX:  randomDestinationX,
+                    destinationY: canvasHeight+enemyRadius,
+                })
+            }
+    }
+    function enemyDownSideGenerator(){
+            if(balls.length){
+                var randomStartX = Math.floor(Math.random() * 1024);
+                var randomDestinationX = Math.floor(Math.random() * 1024);
+                io.sockets.emit('enemy_generator', {
+                    wall : 3,
+                    startingX:  randomStartX,
+                    startingY:  canvasHeight+enemyRadius,
+                    destinationX:  randomDestinationX,
+                    destinationY: enemyRadius,
+                })
+            }
+    }
+
+    function enemyGenerator(){//전 방향 벽에서 총알 생성하게 하는거
+        enemyGeneratorInterval = setInterval(function (){
+            enemyLeftSideGenerator();
+            enemyRightSideGenerator();
+            enemyUpSideGenerator();
+            enemyDownSideGenerator();
+        }, enemyFrequency);
+    }
+
 
     let host = balls[0].id;
     socket.on('start', function(data){
-        isStarted = true;
         if(host == data.id){
-            enemyGenerator = setInterval(function() {
-                if(balls.length){ 
-                    var decideWall = Math.floor(Math.random()*4);
-                    if( decideWall == 0){
-                        var randomStartY = Math.floor(Math.random() * 768)
-                        var randomDestinationY = Math.floor(Math.random() * 768)
-                        io.sockets.emit('enemy_generator', {
-                            wall : 0,
-                            startingX:  enemyRadius,
-                            startingY:  randomStartY,
-                            destinationX:  canvasWidth+enemyRadius,
-                            destinationY: randomDestinationY,
-                        })
-                    }
-                    else if( decideWall == 1){
-                        var randomStartY = Math.floor(Math.random() * 768)
-                        var randomDestinationY = Math.floor(Math.random() * 768)
-                        io.sockets.emit('enemy_generator', {
-                            wall : 1,
-                            startingX:  canvasWidth+enemyRadius,
-                            startingY:  randomStartY,
-                            destinationX:  enemyRadius,
-                            destinationY: randomDestinationY,
-                        })
-                    }
-                    else if( decideWall == 2){
-                        var randomStartX = Math.floor(Math.random() * 1024);
-                        var randomDestinationX = Math.floor(Math.random() * 1024);
-                        io.sockets.emit('enemy_generator', {
-                            wall : 2,
-                            startingX:  randomStartX,
-                            startingY:  enemyRadius,
-                            destinationX:  randomDestinationX,
-                            destinationY: canvasHeight+enemyRadius,
-                        })
-                    }
-                    else if( decideWall == 3){
-                        var randomStartX = Math.floor(Math.random() * 1024);
-                        var randomDestinationX = Math.floor(Math.random() * 1024);
-                        io.sockets.emit('enemy_generator', {
-                            wall : 3,
-                            startingX:  randomStartX,
-                            startingY:  canvasHeight+enemyRadius,
-                            destinationX:  randomDestinationX,
-                            destinationY: enemyRadius,
-                        })
-                    }
-                }
-                }, 1000)
-
+            enemyGenerator();
+            itemGenerator();
+            stageStart();
         }
-        
     })
 
 
@@ -189,79 +207,121 @@ io.on('connection', function(socket) {
             }
         }
         socket.broadcast.emit('collision_update', {id : data.id})
+        isFail = stageFail();
+        if(isFail){
+            io.sockets.emit('game_over', {isFail: true})
+        }
     })
 
     const itemRadius=20;
 
-    socket.on('start', function(data){
-        if(host==data.id){
-            itemGenerator = setInterval(function(){
-                if(balls.length){
-                    var decideWall = Math.floor(Math.random()*4);
-                    if(decideWall == 0){
-                        var randomStartY = Math.floor(Math.random()*76)
+    function itemLeftSideGenerator(){
+        if(balls.length){
+            var randomStartY = Math.floor(Math.random() * 768)
+            var randomDestinationY = Math.floor(Math.random() * 768)
+            io.sockets.emit('item_generator', {
+                wall: 0,
+                startingX: itemRadius,
+                startingY: randomStartY,
+                destinationX: canvasWidth+itemRadius,
+                destinationY: randomDestinationY
+            })
+        }
+    }
 
-                        var randomDestinationY=Math.floor(Math.random())
+    function itemRightSideGenerator(){
+        if(balls.length){
+            var randomStartY = Math.floor(Math.random() * 768)
+            var randomDestinationY = Math.floor(Math.random() * 768)
+            io.sockets.emit('item_generator', {
+                wall: 1,
+                startingX: canvasWidth+itemRadius,
+                startingY: randomStartY,
+                destinationX: itemRadius,
+                destinationY: randomDestinationY
+            })
+        }
+    }
 
-                        io.sockets.emit('item_generator' , {
-                            wall:0,
-                            startingX:itemRadius,
-                            startingX:randomStartX,
-                            startingY:randomStartY,
-                            destinationX: canvasWidth+itemRadius,
-                            destinationY:randomDestinationY,
-                    
-                        })
+    function itemUpSideGenerator(){
+        if(balls.length){
+            var randomStartX = Math.floor(Math.random() * 1024)
+            var randomDestinationX = Math.floor(Math.random() * 1024)
+            io.sockets.emit('item_generator', {
+                wall: 2,
+                startingX: randomStartX,
+                startingY: itemRadius,
+                destinationX: randomDestinationX,
+                destinationY: canvasHeight+itemRadius
+            })
+        }
+    }
 
-                    }
-                    else if( decideWall == 1){
-                        var randomStartY = Math.floor(Math.random() * 768)
-                        var randomDestinationY = Math.floor(Math.random() * 768)
-                        io.sockets.emit('item_generator', {
-                            wall : 1,
-                            startingX:  canvasWidth+itemRadius,
-                            startingY:  randomStartY,
-                            destinationX:  itemRadius,
-                            destinationY: randomDestinationY,
-                        })
-                    }
-                    else if( decideWall == 2){
-                        var randomStartX = Math.floor(Math.random() * 1024);
-                        var randomDestinationX = Math.floor(Math.random() * 1024);
-                        io.sockets.emit('item_generator', {
-                            wall : 2,
-                            startingX:  randomStartX,
-                            startingY:  itemRadius,
-                            destinationX:  randomDestinationX,
-                            destinationY: canvasHeight+itemRadius,
-                        })
-                    }
-                    else if( decideWall == 3){
-                        var randomStartX = Math.floor(Math.random() * 1024);
-                        var randomDestinationX = Math.floor(Math.random() * 1024);
-                        io.sockets.emit('item_generator', {
-                            wall : 3,
-                            startingX:  randomStartX,
-                            startingY:  canvasHeight+itemRadius,
-                            destinationX:  randomDestinationX,
-                            destinationY: itemRadius,
-                        })
-                    }
+    function itemDownSideGenerator(){
+        if(balls.length){
+            var randomStartX = Math.floor(Math.random() * 1024)
+            var randomDestinationX = Math.floor(Math.random() * 1024)
+            io.sockets.emit('item_generator', {
+                wall: 3,
+                startingX: randomStartX,
+                startingY: canvasHeight+itemRadius,
+                destinationX: randomDestinationX,
+                destinationY: itemRadius
+            })
+        }
+    }
+
+    function itemGenerator(){
+        itemGeneratorInterval = setInterval(function(){
+            k = Math.floor(Math.random)*4
+            if(k == 0){
+                itemLeftSideGenerator();
+            }
+            else if(k == 1){
+                itemRightSideGenerator();
+            }
+            else if(k==2){
+                itemUpSideGenerator();
+            }
+            else{
+                itemDownSideGenerator();
+            }
+            
+        }, itemFrequency);
+    }
+
+    function stageClear(){
+        stage = stage + 1;
+        if (enemyFrequency > 200){
+            enemyFrequency -= 100;
+        }
+        clearInterval(enemyGeneratorInterval);
+        enemyGenerator();
+    }
+
+
+    function stageStart(){//스테이지 시작제어, 10초당 1스테이지
+        stageGenerator = setInterval(
+            function(){
+                io.sockets.emit('stage_number', {stage : stage, timer : 10});
+                if(stage <= 8){
+                    stageClear();
                 }
-                }, 4000)
-                    
-                    } 
-                })
+                if(stage == 9){
+                    io.sockets.emit('game_win', {isWin: true});
+                }
+            }
+        , 10000)
 
-    socket.on('item_acquire', function(data){
-        // for(var i =0; i<balls.length; i++){
-        //     if(balls[i].id==data.id){
-        //         balls[i].state=2;
-        //         break;
-        //     }
-        // }
-        socket.broadcast.emit('item_update',{id: data})
-    })
-
+    }
+    function stageFail(){
+        var isFail = true;
+        for(let i = 0 ; i < balls.length ; i++){
+            if(balls[i].state == 1){
+                isFail = false;
+            }
+        }
+        return isFail;
+    }
 })
 
